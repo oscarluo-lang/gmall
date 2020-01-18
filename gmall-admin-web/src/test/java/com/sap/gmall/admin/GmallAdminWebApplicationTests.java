0961689611eb7f1e456521e22215e9df42f75d70
oscarluo-lang <oscar_0921@qq.com>
2020-01-18 11:59:20 +0800

    项目结构创建完成...

---
+++ gmall-admin-web/src/test/java/com/sap/gmall/admin/GmallAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.gmall.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallAdminWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
